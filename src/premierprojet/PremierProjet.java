233337e7d09f3a5f5547218c8d8249c4393be9c1
sergedelil <dogny.gnagnely_serge@courrier.uqam.ca>
2015-01-22 21:07:31 -0500

    Initial commit

---
+++ src/premierprojet/PremierProjet.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package premierprojet;
+import mystere.Mystere;
+/**
+ *
+ * @author sergedelil
+ */
+public class PremierProjet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {//sout suivi de tab
+        System.out.println("Hello Serge");
+        Mystere m = new Mystere();
+        m.methodeMystere();
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        String str = "426.75$";
+        System.out.println(str.substring(6, 7));
+         
+    }
+    
+}
